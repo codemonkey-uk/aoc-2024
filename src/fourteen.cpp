4271f66cadd9e57fefa05216d05c7de255d77925
codemonkey-uk <codemonkey.uk@gmail.com>
2021-12-14 21:35:57 +0000

    day 14 part 2

--- src/fourteen.cpp
+++ src/fourteen.cpp
@@ -8,6 +8,21 @@
 
 using namespace std;
 
+void ReportFreq(const map< char, size_t >& freq)
+{
+    size_t most_common = 0;
+    size_t least_common = numeric_limits<size_t>::max();
+    for (auto f : freq)
+    {
+        size_t i = f.second;
+        cout << f.first << " : " << i << endl;
+        if (i<least_common) least_common = i;
+        if (i>most_common) most_common = i;
+    }
+    size_t diff = most_common-least_common;
+    cout << most_common << " - " << least_common << " = " << diff << endl;
+}
+
 void Fourteen()
 {
     string polymer_template;
@@ -28,53 +43,46 @@ void Fourteen()
         insertions[{a,b}]=c;
     }
     
-    int steps = 10;
+    int steps = 40;
     
     cout << "Template: " << polymer_template << endl;
-    for (int i = 0;i!=steps;++i)
-    {
-        string result;
-        result.reserve( polymer_template.size()*2 );
-        for (size_t c=0;c!=polymer_template.size()-1;++c)
-        {
-            result.push_back(polymer_template[c]);
-            auto itr = insertions.find(
-                {polymer_template[c], polymer_template[c+1]}
-            );
-            if (itr!=insertions.end())
-                result.push_back(itr->second);
-        }
-        result.push_back(polymer_template.back());
-        
-        if (i<4)
-        {
-            cout << "After step " << i+1 << ": " << result << endl;
-        }
-        if (i==4 || i==9)
-        {
-            cout << "After step " << i+1 << " length = " << result.size() << endl;            
-        }
-        
-        polymer_template = result;
-    }
-    
-    int freq[256]={0};
+    map< char, size_t > freq;
     for (unsigned char c : polymer_template)
         freq[c]++;
-
-    int most_common = 0;
-    int least_common = numeric_limits<int>::max();
-    for (int c =0;c!=256;++c )
-    {
-        int i = freq[c];
-        if (i>0)
-        {
-            cout << (char)c << " : " << i << endl;
-            if (i<least_common) least_common = i;
-            if (i>most_common) most_common = i;
-        }
-    }
-    int diff = most_common-least_common;
-    cout << most_common << " - " << least_common << " = " << diff << endl;
     
+    map< pair<char,char>, size_t > pairs;
+    for (size_t c=0;c!=polymer_template.size()-1;++c)
+        pairs[{polymer_template[c], polymer_template[c+1]}]+=1;
+    
+    for (int i = 0;i!=steps;++i)
+    {
+        map< pair<char,char>, size_t > result;
+        for (auto kvp : pairs)
+        {
+            auto itr = insertions.find( kvp.first );
+            if (itr!=insertions.end())
+            {
+                pair<char, char> left = {kvp.first.first,itr->second};
+                pair<char, char> right = {itr->second,kvp.first.second};
+                result[left] += kvp.second;
+                result[right] += kvp.second;
+                freq[itr->second] += kvp.second;
+            }
+            else
+            {
+                result[kvp.first] += kvp.second;
+            }
+        }
+        
+        if (i==11) 
+        {
+            cout << "--- Part 1 (10 steps) ---" << endl;
+            ReportFreq(freq);
+        }
+        
+        pairs = result;
+    }
+    
+    cout << "--- Part 2 (40 steps) ---" << endl;    
+    ReportFreq(freq);
 }
