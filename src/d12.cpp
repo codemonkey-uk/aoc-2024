94a351fdd5b93463f5ced694e5a74170ea1309e7
codemonkey-uk <codemonkey.uk@gmail.com>
2024-12-21 12:23:05 +0000

    d12 p2 solved

--- src/d12.cpp
+++ src/d12.cpp
@@ -12,6 +12,24 @@ typedef uint64_t BigInt;
 typedef Grid<char> Garden;
 typedef Grid<int> Regions;
 
+struct Pos
+{
+    int col;
+    int row;
+};
+
+enum Direction {NORTH, EAST, SOUTH, WEST};
+Pos Offset[] = {{0,-1},{1,0},{0,1},{-1,0}};
+
+// an edge span
+// direction is the direction of the edge away from the inside of the region
+struct Span
+{
+    Pos start;
+    Pos end;
+    Direction dir;
+};
+
 void DebugPrintRegions(const Regions& regions)
 {
     for (int row=0;row!=regions.height;row++)
@@ -57,7 +75,7 @@ Regions FindRegions(const Garden& map, int* regionsOut)
                 // start a new region
                 FloodFill(map, regions, col, row, map.get(col,row), region);
                 region++;
-                DebugPrintRegions(regions);
+                // DebugPrintRegions(regions);
             }
         }
     }
@@ -101,6 +119,62 @@ BigInt Perimeter(int r, const Regions& map)
     return result;
 }
 
+void Insert(Span s, vector<Span>& spans)
+{
+    // insert a span into a list of spans, merging if possible
+    for (auto& span : spans)
+    {
+        if (span.dir==s.dir)
+        {
+            if (span.dir == s.dir)
+            {
+                if ((span.start.col == s.end.col && span.start.row == s.end.row + 1) ||
+                    (span.start.col == s.end.col + 1 && span.start.row == s.end.row))
+                {
+                    span.start = s.start;
+                    return;
+                }
+                if ((span.end.col == s.start.col && span.end.row == s.start.row - 1) ||
+                    (span.end.col == s.start.col - 1 && span.end.row == s.start.row))
+                {
+                    span.end = s.end;
+                    return;
+                }
+            }
+        }
+    }
+    spans.push_back(s);
+}
+
+BigInt Sides(int r, const Regions& map)
+{
+    // count the number of sides of the perimeter of a given region
+    vector<Span> spans;
+
+    for (int row=0;row!=map.height;row++)
+    {
+        for (int col=0;col!=map.width;col++)
+        {
+            if (map.get(col,row)==r)
+            {
+                if (col==0 || map.get(col-1,row)!=r) 
+                    Insert({{col,row},{col,row},WEST}, spans);
+
+                if (col==map.width-1 || map.get(col+1,row)!=r) 
+                    Insert({{col,row},{col,row},EAST}, spans);
+
+                if (row==0 || map.get(col,row-1)!=r) 
+                    Insert({{col,row},{col,row},NORTH}, spans);
+
+                if (row==map.height-1 || map.get(col,row+1)!=r) 
+                    Insert({{col,row},{col,row},SOUTH}, spans);
+            }
+        }
+    }
+
+    return spans.size();
+}
+
 void Twelve()
 {
     string line;
@@ -123,13 +197,19 @@ void Twelve()
 
     cout << regionCount-1 << endl;
     
-    BigInt sum=0;
+    BigInt p1sum=0;
+    BigInt p2sum=0;
     for (int r = 1; r!=regionCount; ++r)
     {
         BigInt a = Area(r, regionsMap);
         BigInt p = Perimeter(r, regionsMap);
-        BigInt price = a*p;
-        sum += price;
-        cout << r << ": "<< a << " x " << p << " = " << price << " = " << sum << endl;
+        BigInt s = Sides(r, regionsMap);
+        
+        p1sum += a*p;
+        p2sum += a*s;
+        cout << r << ": "<< a << " tiles " << p << " permiter " << s << " sides " << endl;
     }
+    
+    cout << "P1: " << p1sum << endl;
+    cout << "P2: " << p2sum << endl;
 }
